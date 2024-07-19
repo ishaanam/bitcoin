b50809c7786ae6b1a6c50866bd10bc59c7ff74c2
ishaanam <ishaana.misra@gmail.com>
2024-07-19 10:45:40 -0400

    add coinjoins util

---
+++ src/util/coinjoins.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_COINJOINS_H
+#define BITCOIN_UTIL_COINJOINS_H
+
+#endif // BITCOIN_UTIL_COINJOINS_H
